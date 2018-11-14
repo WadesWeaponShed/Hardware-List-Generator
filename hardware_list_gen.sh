1e009361fb2b0d7ce09a7a8f8d6092d1c90c6516
Adam Forester <aforester@silverdogstudios.com>
2018-11-14 16:14:13 -0600

    Changed to CSV

--- hardware_list_gen.sh
+++ hardware_list_gen.sh
@@ -6,10 +6,10 @@ printf "There are $total objects\n"
 
 for I in $(seq 0 500 $total)
 do
- mgmt_cli -r true -d $DOMAIN show objects type "simple-gateway" details-level full offset $I limit 500 --format json | jq --raw-output '.objects[] | .name + " " + .hardware + .hwName + " " + .type + " " + ."ipv4-address"' >> $DOMAIN-hardware-list.txt
+ mgmt_cli -r true -d $DOMAIN show objects type "simple-gateway" details-level full offset $I limit 500 --format json | jq --raw-output '.objects[] | .name + "," + .hardware + .hwName + "," + .type + "," + ."ipv4-address"' >> $DOMAIN-hardware-list.txt
 done
 
-sed -i '1s/^/NAME HARDWARE TYPE MGMT_IP\n/' $DOMAIN-hardware-list.txt
+sed -i '1s/^/NAME,HARDWARE,TYPE,MGMT_IP\n/' $DOMAIN-hardware-list.txt
 sed -i 's/CpmiGatewayCluster/Cluster/g' $DOMAIN-hardware-list.txt
 sed -i 's/simple-gateway/Standalone/g' $DOMAIN-hardware-list.txt
 sed -i 's/CpmiVsClusterNetobj/VS/g' $DOMAIN-hardware-list.txt
